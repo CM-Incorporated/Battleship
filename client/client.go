d53d09c380515454d87414d8409c967da3620ada
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2026-08-17 21:23:30 +0100

    feat: working web sockets example

--- client/client.go
+++ client/client.go
@@ -1,1 +1,35 @@
-package client
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/coder/websocket"
+	"github.com/coder/websocket/wsjson"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+	c, _, err := websocket.Dial(ctx, "ws://localhost:8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer c.CloseNow()
+
+	for i := 0; i < 100; i++ {
+		err = wsjson.Write(ctx, c, i)
+		if err != nil {
+			log.Fatal("Oh god")
+		}
+
+	}
+
+	err = wsjson.Write(ctx, c, "close")
+	if err != nil {
+		log.Fatal("Oh god")
+	}
+
+	c.Close(websocket.StatusNormalClosure, "")
+}

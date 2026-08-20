3a72457bc2eb0b42f45a0e7334200cb0204a4fe6
Michael Wiciak <86964604+MichaelWiciak@users.noreply.github.com>
2026-08-20 22:27:33 +0100

    chore: skeleton of canPlace function

--- gamecode/gamecode.go
+++ gamecode/gamecode.go
@@ -1,10 +1,17 @@
 package gamecode
 
 import (
+	"fmt"
+
 	"cmincorporated.com/protocol"
 )
 
 func PlaceShips(seed int64) (protocol.Board, []protocol.Ship) {
 	// Create board, place all 5 ships at random
 	var board protocol.Board = xxx
+}
+
+func canPlace(protocol.Board, protocol.Coord) bool {
+	fmt.Printf("Yo")
+	return true
 }
